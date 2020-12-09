ce7861a4a88d4af72b1a90305930922416f5d7f5
Mathilde <math_dahl@hotmail.com>
2020-12-09 16:49:17 +0100

    useless comment removed

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -16,8 +16,6 @@ import java.util.Set;
 @Controller
 public class HomeController {
 
-    //hej
-
     private UserService userService; // Spring vil selv komme med objektet hertil
 
     public HomeController(UserService userService) {
