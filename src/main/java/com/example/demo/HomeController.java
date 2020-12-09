ae6a2325c5b948e561f9197206d2da04db6f6fec
Mathilde <math_dahl@hotmail.com>
2020-12-09 18:05:49 +0100

    post tag d

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -16,8 +16,6 @@ import java.util.Set;
 @Controller
 public class HomeController {
 
-    //test
-
     private UserService userService; // Spring vil selv komme med objektet hertil
 
     public HomeController(UserService userService) {
